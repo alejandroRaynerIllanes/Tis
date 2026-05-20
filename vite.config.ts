0c0e63512d73030238e2f98aba628e359c347bfb
alejandroRaynerIllanes <alejandroillanes42@gmail.com>
2026-05-20 19:37:22 -0400

    chore: limpieza de codigo muerto y reubicacion de assets

--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,19 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-function figmaAssetResolver() {
-  return {
-    name: 'figma-asset-resolver',
-    // Usamos id: string para evitar errores de tipo
-    resolveId(id: string) {
-      if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
-      }
-      return null
-    }
-  }
-}
-
 export default defineConfig({
-  plugins: [figmaAssetResolver(), react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+
   resolve: {
     alias: {
-      // Alias @ to the src directory
       '@': path.resolve(__dirname, './src')
     }
   },
-  // --- Límite de advertencia agregado por Gustavo ---
-  build: {
-    chunkSizeWarningLimit: 1000
-  },
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
+
+  // File types to support raw imports.
+  // Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv']
 })

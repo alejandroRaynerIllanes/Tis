d254d3f150193e8c19a3ab208ee90103bfd28b51
alejandroRaynerIllanes <alejandroillanes42@gmail.com>
2026-05-20 19:59:19 -0400

    chore: eliminar dependencias innecesarias y optimizar bundle

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
 
   resolve: {
     alias: {
